79b54d0873bee5e3442b08d9a07f18cef01f8941
Camilo Mazorra <mazorra006@gmail.com>
2025-04-21 12:15:46 -0500

    nat, igw, eip

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -30,3 +30,29 @@ resource "aws_subnet" "db_subnet_3tier" {
   availability_zone = var.vpc_azs[count.index]
   tags              = merge(var.general_tags, { "Name" = "subnet_db_0${count.index + 1}" })
 }
+
+# Internet Gateway
+resource "aws_internet_gateway" "IGW_3tier" {
+  vpc_id = aws_vpc.vpc_3tier.id
+  tags   = merge(var.general_tags, { "Name" = "IGW_main" })
+}
+
+resource "aws_eip" "EIP_3tier" {
+  domain = "vpc"
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "NAT_3tier" {
+  allocation_id = aws_eip.EIP_3tier.id
+  subnet_id     = aws_subnet.public_subnet_3tier[0].id
+  tags          = merge(var.general_tags, { "Name" = "NAT_main" })
+}
+
+# Tabla de enrutamiento subredes privadas
+resource "aws_route_table" "private_subnet_RTB_3tier" {
+  vpc_id = aws_vpc.vpc_3tier.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.NAT_3tier.id
+  }
+}
